ebd39c3377f275995a5668c0acc8e4d9fa27edf0
Henrique Anzilago <88406845+Anzilago@users.noreply.github.com>
2024-09-16 15:28:29 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Integer, String, Boolean, Column
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-db = create_engine("sqlite:///database/meubanco.db")
+db = create_engine("sqlite:///meubanco.db")
 Session =sessionmaker(bind=db)
 session = Session()
 
