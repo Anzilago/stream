657d23a860805e35627e7b28038c6532ef7a5616
Henrique Anzilago <88406845+Anzilago@users.noreply.github.com>
2024-09-16 21:14:12 -0400

    Add files via upload

--- homepage.py
+++ homepage.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.title("Homepage")
+secao_usuario = st.session_state
+nome_usuario = None
+if "username" in secao_usuario:
+    nome_usuario = secao_usuario.name
+
+coluna_esquerda, coluna_direita = st.columns([1, 1.5])
+
+if nome_usuario:
+    coluna_esquerda.title(f"Bem vindo, {nome_usuario}!")
+
+botao_dashboards = coluna_esquerda.button("Dashboards Projetos")
+botao_indicadores = coluna_esquerda.button("Principais Indicadores")
+
+if botao_dashboards:
+    st.switch_page("dashboard.py")
+if botao_indicadores:
+    st.switch_page("indicadores.py")
+
+coluna_direita.image("fattura.png")
